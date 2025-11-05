ca8d64fd842ffd77b1b07f86dfbda42747e87648
Limitex <76650151+Limitex@users.noreply.github.com>
2025-11-05 22:37:41 +0900

    fix: clip domain

--- src/nodes/_diffusers_clip_text_encode.py
+++ src/nodes/_diffusers_clip_text_encode.py
@@ -29,6 +29,9 @@ class DiffusersClipTextEncode:
         text: str,
         handler: ClipTextEncodeHandler = Provide[Container.clip_text_encode_handler],
     ) -> tuple[ComfyUIConditioningDTO]:
-        conditioning_model = handler.encode(clip.tokenizer, clip.text_encoder, clip.path, text)
+        clip_domain = ComfyUIClipDTO.to_domain(clip)
+        conditioning_model = handler.encode(
+            clip_domain.tokenizer, clip_domain.text_encoder, clip_domain.path, text
+        )
         conditioning_dto = ComfyUIConditioningDTO.from_domain(conditioning_model)
         return (conditioning_dto,)
