f7d6e25766c86cd072aede4d847895d730118bd4
Limitex <76650151+Limitex@users.noreply.github.com>
2025-11-09 06:55:17 +0900

    feat: get project root config

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,4 +1,5 @@
 from ._cache import get_cache_dir
 from ._env import load_envs
+from ._path import get_project_root
 
-__all__ = ["load_envs", "get_cache_dir"]
+__all__ = ["load_envs", "get_cache_dir", "get_project_root"]
